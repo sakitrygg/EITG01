10c8dcf21750280abce549194e0e270721acb02a
Isac Karlsson <karlsson.isac@gmail.com>
2022-11-09 13:54:29 +0100

    Application Project

---
+++ ApplictionProject/ApplictionProject/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using ApplictionProject.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace ApplictionProject.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+
+
+        }
+    }
+}
---
+++ ApplictionProject/ApplictionProject/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using ApplictionProject.Models;
+using ApplictionProject.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ApplictionProject.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
